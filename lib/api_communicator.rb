1a04e4e65f66954d8d34e1a63011a0691319f471
github username <github email address>
2019-01-29 15:03:14 -0500

    sharon api

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -7,26 +7,40 @@ def get_character_movies_from_api(character_name)
   response_string = RestClient.get('http://www.swapi.co/api/people/')
   response_hash = JSON.parse(response_string)
 
-  # iterate over the response hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `print_movies`
-  #  and that method will do some nice presentation stuff like puts out a list
-  #  of movies by title. Have a play around with the puts with other info about a given film.
+    film_array = []
+  response_hash["results"].each do |character_hash| #response_hash["results"] is an array
+
+    if character_name == character_hash['name']
+
+     character_hash["films"].each do |film|
+       film_string = RestClient.get(film)
+       films = JSON.parse(film_string)
+
+       film_array << films
+         # puts film["title"]
+      end
+    end
+  end
+  film_array
 end
 
-def print_movies(films)
-  # some iteration magic and puts out the movies in a nice list
+ def print_movies(films)
+
+   # puts films["title"]
+   films.each_with_index do |film, i|
+   puts "#{i +1} #{film["title"]}"
+   # binding.pry
+# #   # some iteration magic and puts out the movies in a nice list
 end
+ end
 
 def show_character_movies(character)
   films = get_character_movies_from_api(character)
   print_movies(films)
 end
 
+character_name = "Luke Skywalker"
+show_character_movies(character_name)
 ## BONUS
 
 # that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
